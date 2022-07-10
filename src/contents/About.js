0818b6fb50de077dfbb01977acc2be2908e3f0c6
Ram-debug <amishraj2468@gmail.com>
2022-07-10 21:30:46 +0530

    Initial Commit

---
+++ src/contents/About.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+
+class About extends Component {
+    render() {
+        return (
+            <div className="condiv">
+            <h1 className="subtopic">About Me</h1>
+            <h4>Hey there,</h4>
+            <h1>I'm Ram Kumar</h1>
+            <h3>Data science <u>student</u> | JAVA <u>Programmer</u></h3>
+            <br></br>
+            <p>I am interest in computer science at very early age, currently I am 
+                Pursuing B. Tech from Lovely Professional University. Data science 
+                is my center of interest and do practice regular coding over online platform 
+                like GFG and HackerRank, moreover I am 5 star rating coder in 
+                problem solving at HackerRank. Apart from my technical skills 
+                I am a team player and adaptable in nature. I am looking to explore new technologies and use
+                my skills to solve real life problems.           
+            </p>            
+            </div>
+            )
+        }
+    }
+    
+export default About
+    
